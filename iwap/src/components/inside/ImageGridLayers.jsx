18d4e5009db30f9da9bbbf14c0106912cdf0fee3
김경록 <gyonglok0417@naver.com>
2025-10-07 19:54:51 +0900

    [feat] inside 숫자 레이어 글꼴 적용
    react-three/drei 에서 woff2를 지원하지 않음. ttf를 추가하여 글꼴 적용

--- iwap/src/components/inside/ImageGridLayers.jsx
+++ iwap/src/components/inside/ImageGridLayers.jsx
@@ -53,7 +53,9 @@ function Scene({ layers, animatedFocusIndex, rotation, opacity }) {
           
           return layer.isTextLayer ? (
             <animated.group position={position} key={layer.id}>
-              <Text fontSize={100} color="white" anchorX="center" anchorY="middle">
+              <Text 
+                font="/fonts/PretendardVariable.ttf"
+                fontSize={100} color="white" anchorX="center" anchorY="middle">
                 {layer.text}
                 <animated.meshStandardMaterial color="white" opacity={opacity} transparent />
               </Text>
