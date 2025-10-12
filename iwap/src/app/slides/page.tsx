f9c27170a7de4585783b0ea8e52c2e318740fa2e
김경록 <gyonglok0417@naver.com>
2025-10-13 01:01:32 +0900

    [fix] 슬라이드 제목 및 설명 수정

--- iwap/src/app/slides/page.tsx
+++ iwap/src/app/slides/page.tsx
@@ -5,10 +5,10 @@ import { usePathname } from "next/navigation";
 import { CardSlider } from "@/components/slides/CardSlider"; // 경로에 맞게 수정
 
 const images = [
-  { src: "/images/home/slides/slide1.jpg", link: "/inside", text: "!nside", description: "This is the description for Str!ng." },
-  { src: "/images/home/slides/slide2.jpg", link: "/this-is-for-u", text: "Th!s !s for u", description: "A look at the inner world." },
-  { src: "/images/home/slides/slide3.jpg", link: "/piano", text: "P!ano", description: "Interactive piano project." },
-  { src: "/images/home/slides/slide4.jpg", link: "/string", text: "Str!ng", description: "A special project for you." },
+  { src: "/images/home/slides/slide1.jpg", link: "/inside", text: "!nside", description: "인공지능이 숫자를 인식하는 과정" },
+  { src: "/images/home/slides/slide2.jpg", link: "/this-is-for-u", text: "Th!s !s for u", description: "함수로 하트 그리기" },
+  { src: "/images/home/slides/slide3.jpg", link: "/piano", text: "P!ano", description: "음성을 피아노로 변환" },
+  { src: "/images/home/slides/slide4.jpg", link: "/string", text: "Str!ng", description: "네 번째 작품에 대한 설명입니다." },
   { src: "/images/home/slides/slide5.jpg", link: "/slides/5", text: "작품 5", description: "다섯 번째 작품에 대한 설명입니다." },
   { src: "/images/home/slides/slide6.jpg", link: "/slides/6", text: "작품 6", description: "여섯 번째 작품에 대한 설명입니다." },
 ];
