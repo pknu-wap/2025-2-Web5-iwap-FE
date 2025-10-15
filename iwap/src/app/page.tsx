a08e699a3c7ab1b50b53f646043a2b26ef3a995d
김경록 <gyonglok0417@naver.com>
2025-10-15 11:27:46 +0900

    [fix] 반응형 레이아웃 적용 #28

--- iwap/src/app/page.tsx
+++ iwap/src/app/page.tsx
@@ -102,21 +102,24 @@ export default function Home() {
       <Image src="/images/home_background.jpg" alt="Background Light" fill priority className="object-cover dark:hidden" />
       <Image src="/images/home-black_background.jpg" alt="Background Dark" fill priority className="object-cover hidden dark:block" />
 
-      {/* 애니메이션 컨테이너 (텍스트 + 버튼) */}
-      <div className="absolute inset-0 z-20 flex items-center justify-center -translate-y-5 px-4 sm:justify-start sm:ml-[15vw] md:ml-[20vw]">
+      {/* padding 설정 컨테이너 */}
+      <div className="absolute inset-0 z-20 flex items-center justify-center -translate-y-5 px-6 sm:px-8 md:px-12">
         
         {/* 애니메이션 그룹 */}
         <div
-          className={`flex flex-col sm:flex-row items-center sm:items-start gap-8 transition-transform duration-700 ease-out ${
-            isOpen ? "sm:-translate-x-20" : ""
+           className={`flex w-full max-w-screen-xl 
+            flex-col items-center gap-12                  /* 모바일 기본: 세로, 가운데 정렬 */
+            sm:flex-row sm:items-end sm:justify-between   /* sm 이상: 가로, 하단 정렬, 양쪽 끝 정렬 */
+            transition-transform duration-700 ease-out ${
+            isOpen ? "sm:-translate-x-10" : ""            /* 가로 이동은 sm 이상에서만 적용 */
           }`}
         >
-          {/* 텍스트 영역 */}
-          <div className="flex flex-col items-center sm:items-start">
-            <Image src="/images/home/wap.png" alt="images/home/wap" width={148} height={45} className="w-[148px] h-[45px] mb-4 sm:mb-0" />
+          {/* 텍스트 블록 (그룹 좌측) */}
+          <div className="flex flex-col items-start">
+            <Image src="/images/home/wap.png" alt="images/home/wap" width={148} height={45} className="w-[148px] h-[45px] mb-4" />
             <h1
               ref={h1Ref}
-              className="relative text-white text-center sm:text-left whitespace-pre-line text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[128px] cursor-default pb-2"
+              className="relative text-white text-left whitespace-pre-line text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[128px] cursor-default pb-2"
               style={{
                 fontFamily: "Pretendard",
                 fontWeight: 700,
@@ -132,10 +135,11 @@ export default function Home() {
             </h1>
           </div>
 
-          {/* 시각적 버튼 */}
+          {/* 시각적 버튼 (그룹 우측) */}
           <div
-            ref={buttonRef} 
-            className="relative w-[240px] h-[70px] sm:w-[280px] sm:h-[80px] sm:self-end sm:mb-2 lg:mb-10 pointer-events-none">
+            ref={buttonRef}
+            className="relative w-[240px] h-[70px] sm:w-[280px] sm:h-[80px] pointer-events-none self-center sm:self-end mb-2 lg:mb-10 flex-shrink-0" // flex-shrink-0 추가로 버튼이 찌그러지는 것을 방지
+          >
             <div className="absolute inset-0 flex items-center justify-center">
               <button className="relative flex items-center justify-center w-full h-full gap-2 bg-transparent">
                 <svg className="w-full" height="4" viewBox="0 0 490 4" fill="none">
@@ -151,6 +155,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+
       
       {/* 고정된 호버 인식 및 링크 영역 */}
       {buttonRect && (
@@ -162,7 +167,7 @@ export default function Home() {
             top: `${buttonRect.top - 50}px`,         // 세로 시작점
             height: `${buttonRect.height + 100}px`,  // 세로 높이
 
-            left: `${buttonRect.left - 100}px`,       // 가로 시작점
+            left: `${buttonRect.left - 100}px`,      // 가로 시작점
             width: `${buttonRect.width + 250}px`,    // 가로 너비
           }}
         >
