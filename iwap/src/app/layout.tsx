10145002e054feb22e9ab62257b54223754f9cf8
김경록 <gyonglok0417@naver.com>
2025-10-28 17:54:31 +0900

    [fix] layout 헤더 높이 수정

--- iwap/src/app/layout.tsx
+++ iwap/src/app/layout.tsx
@@ -22,18 +22,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         
         {/* (메인페이지가 아닌 경우) 모바일(기본)에서는 숨기고, md 사이즈 이상일 때만 flex로 표시 */}
         {showHeader && (
-          <header className="hidden w-full h-[96px] bg-white md:flex flex-col items-center justify-center fixed top-0 left-0 z-50">
-            <h1 className="text-black text-3xl font-semibold">
+          <header className="hidden w-full h-[60px] bg-white md:flex flex-col items-center justify-center fixed top-0 left-0 z-50">
+            <h1 className="text-black text-2xl font-semibold">
               !WAP
             </h1>
-            <p className="text-black text-base font-extralight">
+            <p className="text-black text-base font-extralight -translate-y-0.5">
               !nteractive Web Art Project
             </p>
           </header>
         )}
         
         {/* (메인페이지가 아닌 경우) md 사이즈 이상일 때만 헤더 높이만큼 상단 패딩 적용 */}
-        <main className={showHeader ? "md:pt-[96px]" : ""}>
+        <main className={showHeader ? "md:pt-[60px]" : ""}>
           {children}
         </main>
       </body>
