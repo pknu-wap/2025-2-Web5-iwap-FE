972ec16fb63485bbccd4b94d10212c6014633cd1
Kim nayoung <kny4345@gmail.com>
2025-10-31 22:24:34 +0900

    [chore] slide - 불필요한 파일 삭제

--- iwap/src/components/slides/ClientSlider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-const HeroSlider = dynamic(() => import("@/components/slides/HeroSlider"), { ssr: false });
-
-export default function ClientSlider() {
-  return <HeroSlider />;
-}
