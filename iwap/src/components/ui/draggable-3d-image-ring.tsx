d3d5c2a6ceb08468d05d56b8c3e88677f932234d
doranayoung <kny4345@gmail.com>
2025-11-02 18:02:33 +0900

    [fix] main - vercel 배포시 module 에러 수정

--- iwap/src/components/ui/draggable-3d-image-ring.tsx
+++ iwap/src/components/ui/draggable-3d-image-ring.tsx
@@ -1,5 +1,3 @@
-export {
-  ThreeDImageRing,
-  type ThreeDImageRingProps,
-  type ThreeDImageInput,
-} from "@/components/lightswind/3d-image-ring";
+import ThreeDImageRing from "@/components/lightswind/3d-image-ring";
+
+export { ThreeDImageRing };
