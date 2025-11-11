cecde88a4ff555713ac6e93caff9e2ced48defdf
doranayoung <kny4345@gmail.com>
2025-11-11 10:56:07 +0900

    [feat] Landing - 소개말 추가

--- iwap/src/app/Landing/page.tsx
+++ iwap/src/app/Landing/page.tsx
@@ -5,6 +5,7 @@ import { CardSlider } from "@/components/slides/CardSlider";
 import { motion } from "framer-motion";
 import SideImageSection from "@/components/sections/SideImageSection";
 
+
 const container = { hidden: { opacity: 0 }, show: { opacity: 1, transition: { staggerChildren: 0.08 } } };
 const item = { hidden: { opacity: 0, y: 12 }, show: { opacity: 1, y: 0, transition: { duration: 0.6 } } };
 
@@ -67,22 +68,124 @@ export default function IwapLanding() {
         />
       </div>
 
-      <SideImageSection
-id="subproject"
-imageSrc="/images/inside_background.jpg"
-imageAlt="!nside background"
-side="left"
-heading="01 !nside">
+      <Section id="about" className="py-16 scroll-mt-24">
+        <motion.div variants={container} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0.2 }}>
+          <div className="relative">
+            <motion.p variants={item} className="text-center text-[#6670BD] text-[30px]">
+              <br />
+              <span className="font-semibold text-[42px]">!WAP</span>
+          <span> 은</span>
+          <span className="font-semibold"> 시각, 청각, 촉각</span>
+          <span>의 세 감각을</span><br />
+          <span>웹이라는 매체 안에서 새롭게 해석하는</span>
+          <span className="font-semibold"> 인터랙티브 프로젝트</span>
+          <span> 입니다.</span>
+            </motion.p>
+          </div>
+          </motion.div>        
+      </Section>
 
-<span>인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
-<br /><br />
-<span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
-<span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
-<br />
-<span>사용자는 화면 위에서 숫자를 그리면, 인공지능의 내부 feature</span><br />
-<span> map이 실시간으로 나타나며</span><br />
-<span>기계의 시각적 사고 과정을 체험할 수 있습니다.</span>
-</SideImageSection>
+      <SideImageSection
+        id="subproject"
+        imageSrc="/images/inside_background.jpg"
+        imageAlt="!nside background"
+        side="left"
+        heading="01 !nside">
+
+        <span>인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
+        <br /><br />
+        <span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
+        <span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
+        <br />
+        <span>사용자는 화면 위에서 숫자를 그리면, 인공지능의 내부 feature</span><br />
+        <span> map이 실시간으로 나타나며</span><br />
+        <span>기계의 시각적 사고 과정을 체험할 수 있습니다.</span>
+      </SideImageSection>
+
+      <SideImageSection
+        id="subproject"
+        imageSrc="/images/This-is-for-u_background.jpg"
+        imageAlt="!nside background"
+        side="right"
+        heading="01 !nside">
+
+        <span>인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
+        <br /><br />
+        <span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
+        <span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
+        <br />
+        <span>사용자는 화면 위에서 숫자를 그리면, 인공지능의 내부 feature</span><br />
+        <span> map이 실시간으로 나타나며</span><br />
+        <span>기계의 시각적 사고 과정을 체험할 수 있습니다.</span>
+      </SideImageSection>
+
+      <SideImageSection
+        id="subproject"
+        imageSrc="/images/inside_background.jpg"
+        imageAlt="!nside background"
+        side="left"
+        heading="01 !nside">
+
+        <span>인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
+        <br /><br />
+        <span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
+        <span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
+        <br />
+        <span>사용자는 화면 위에서 숫자를 그리면, 인공지능의 내부 feature</span><br />
+        <span> map이 실시간으로 나타나며</span><br />
+        <span>기계의 시각적 사고 과정을 체험할 수 있습니다.</span>
+      </SideImageSection>
+
+      <SideImageSection
+        id="subproject"
+        imageSrc="/images/This-is-for-u_background.jpg"
+        imageAlt="!nside background"
+        side="right"
+        heading="01 !nside">
+
+        <span>인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
+        <br /><br />
+        <span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
+        <span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
+        <br />
+        <span>사용자는 화면 위에서 숫자를 그리면, 인공지능의 내부 feature</span><br />
+        <span> map이 실시간으로 나타나며</span><br />
+        <span>기계의 시각적 사고 과정을 체험할 수 있습니다.</span>
+      </SideImageSection>
+
+      <SideImageSection
+        id="subproject"
+        imageSrc="/images/inside_background.jpg"
+        imageAlt="!nside background"
+        side="left"
+        heading="01 !nside">
+
+        <span>인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
+        <br /><br />
+        <span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
+        <span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
+        <br />
+        <span>사용자는 화면 위에서 숫자를 그리면, 인공지능의 내부 feature</span><br />
+        <span> map이 실시간으로 나타나며</span><br />
+        <span>기계의 시각적 사고 과정을 체험할 수 있습니다.</span>
+      </SideImageSection>
+
+      <SideImageSection
+        id="subproject"
+        imageSrc="/images/This-is-for-u_background.jpg"
+        imageAlt="!nside background"
+        side="right"
+        heading="01 !nside">
+
+        <span>인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
+        <br /><br />
+        <span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
+        <span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
+        <br />
+        <span>사용자는 화면 위에서 숫자를 그리면, 인공지능의 내부 feature</span><br />
+        <span> map이 실시간으로 나타나며</span><br />
+        <span>기계의 시각적 사고 과정을 체험할 수 있습니다.</span>
+      </SideImageSection>      
 
       <footer className="border-t border-gray-200 py-8 bg-white">
         <Section className="flex flex-col md:flex-row items-center justify-between gap-4 text-gray-600 text-sm">
