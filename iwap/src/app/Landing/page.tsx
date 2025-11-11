822261e895d7ee0c40a9e9c89d278b0f1bad36b5
doranayoung <kny4345@gmail.com>
2025-11-11 16:35:26 +0900

    [feat] Landing - 형광펜 요소 추가 + image 안에 글자 추가

--- iwap/src/app/Landing/page.tsx
+++ iwap/src/app/Landing/page.tsx
@@ -54,7 +54,7 @@ export default function IwapLanding() {
   return (
     <div className="min-h-screen w-full bg-white text-black">
 
-      <div className="mt-40">
+      <div className="mt-20">
         <CardSlider
           showHeader={false}
           images={[
@@ -67,6 +67,10 @@ export default function IwapLanding() {
           ]}
         />
       </div>
+
+      <Section id="about" className="py-16 scroll-mt-24 text-center">
+        <span className="font-Light text-[36px]">Explore the Senses</span>      
+      </Section>
 
       <Section id="about" className="py-16 scroll-mt-24">
         <motion.div variants={container} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0.2 }}>
@@ -88,13 +92,19 @@ export default function IwapLanding() {
       <SideImageSection
         id="subproject"
         imageSrc="/images/inside_background.jpg"
-        imageAlt="!nside background"
+        imageAlt="!nside background" 
         side="left"
+        {...{ imageOverlay: "시작하기" }}
+        imageOverlayLeft={"!nside"}
+        overlayLeftClassName = "-translate-x-14 -translate-y-25"
+        imageOverlayRight={"01"}
         badgeText="Vision"
         textAlign="left"
         badgeAlign="left"
         heading="01 !nside">
-        <span>인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
+
+        <span className="relative inline-block after:content-[''] after:absolute after:inset-x-0 after:bottom-0 after:h-[22px] after:bg-[rgba(157,157,197,0.39)] after:-z-10 relative z-10">
+        인공지능의 ‘시각’을 인간의 감각 언어로 번역하는 실험.</span>
         <br /><br />
         <span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
         <span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
@@ -108,15 +118,20 @@ export default function IwapLanding() {
         id="subproject"
         imageSrc="/images/This-is-for-u_background.jpg"
         imageAlt="This-is-for-u background"
+        {...{ imageOverlay: "시작하기" }}
+        imageOverlayLeft={"Th!s !s for u"}
+        overlayLeftClassName = "-translate-y-7 -translate-x-37"
+        imageOverlayRight={"02"}
         side="right"
         badgeText="Vision"
         textAlign="right"
         badgeAlign="right"
         heading="02  Th!s !s for u">
 
-        <span>감정을 색과 형태로 번역하는 ‘디지털 엽서’.</span>
+        <span className="relative inline-block after:content-[''] after:absolute after:inset-x-0 after:bottom-0 after:h-[22px] after:bg-[rgba(157,157,197,0.39)] after:-z-10 relative z-10">
+        감정을 색과 형태로 번역하는 ‘디지털 엽서’.</span>
         <br /><br />
-        <span>인간의 마음이 색채 반응으로 드러나는 시각적으로 체험합니다.</span><br />
+        <span>인간의 마음이 색채 반응으로 드러나는 시각적으로 체험합니다.</span><br /><br />
         <span>사용자가 손으로 하트를 그리고, 글로 메시지를 남길 수 있습니다.</span><br />
         <span>그림의 색, 곡선의 흐름에 따라 감정이 시각적으로 표현되며,</span><br />
         <span>완성된 엽서는 이미지로 저장되거나 이메일로 전송됩니다.</span>
@@ -126,6 +141,10 @@ export default function IwapLanding() {
         id="subproject"
         imageSrc="/images/Piano_Landing.png"
         imageAlt="Piano background"
+        {...{ imageOverlay: "시작하기" }}
+        imageOverlayLeft={"P!ano"}
+        overlayLeftClassName = "-translate-x-14 -translate-y-25"
+        imageOverlayRight={"03"}
         side="right"
         badgeText="Hearing"
         textAlign="left"
@@ -134,26 +153,31 @@ export default function IwapLanding() {
 
         <span>사용자의 음성이나 주변 소리를 피아노 음으로 변환하는</span>
         <br />
-        <span>사운드–비주얼 인터랙션 실험.</span><br /><br />
-        <span>AI가 숫자(0-9)를 인식하는 과정을 시각화하여,</span><br />
-        <span>기계가 세상을 ‘본다’는 것이 무엇인지 탐구합니다.</span><br />
+        <span className="relative inline-block after:content-[''] after:absolute after:inset-x-0 after:bottom-0 after:h-[22px] after:bg-[rgba(157,157,197,0.39)] after:-z-10 relative z-10">
+        사운드–비주얼 인터랙션 실험.</span><br /><br />
+        <span>음성 입력을 Tone.js 기반의 피아노 사운드로 변환하고,</span><br />
+        <span>각 음정과 강도를 시각적으로 스펙트럼화하여 보여줍니다.</span><br />
         <br />
-        <span>사용자는 화면 위에서 숫자를 그리면, 인공지능의 내부 feature</span><br />
-        <span> map이 실시간으로 나타나며</span><br />
-        <span>기계의 시각적 사고 과정을 체험할 수 있습니다.</span>
+        <span>시청각이 결합된 몰입형 음악 인터페이스로,</span><br />
+        <span>“보이는 소리”라는 개념을 체험할 수 있습니다.</span><br />
       </SideImageSection>
 
       <SideImageSection
         id="subproject"
         imageSrc="/images/ascii_background.jpg"
         imageAlt="ascii background"
+        {...{ imageOverlay: "시작하기" }}
+        imageOverlayLeft={"Asci!"}
+        overlayLeftClassName = "-translate-x-10 -translate-y-30"
+        imageOverlayRight={"04"}
         side="left"
         badgeText="Vision"
         textAlign="left"
         badgeAlign="left"
         heading="04  Asci!">
 
-        <span>이미지를 텍스트로 변환해 ‘보이는 언어’를 만드는 실험.</span>
+        <span className="relative inline-block after:content-[''] after:absolute after:inset-x-0 after:bottom-0 after:h-[22px] after:bg-[rgba(157,157,197,0.39)] after:-z-10 relative z-10">
+        이미지를 텍스트로 변환해 ‘보이는 언어’를 만드는 실험.</span>
         <br /><br />
         <span>이미지를 픽셀 단위로 분석해, 밝기 값을 문자(ASCII 코드)로 대체합니다.</span><br />
         <span>그 결과 생성된 텍스트 이미지는 언어와 시각 사이의 경계를 흐리며,</span><br />
@@ -164,13 +188,18 @@ export default function IwapLanding() {
         id="subproject"
         imageSrc="/images/string_background.jpg"
         imageAlt="string background"
+        {...{ imageOverlay: "시작하기" }}
+        imageOverlayLeft={"Str!ng"}
+        overlayLeftClassName = "-translate-x-14 -translate-y-25"
+        imageOverlayRight={"05"}
         side="left"
         badgeText="Vision"
         textAlign="left"
         badgeAlign="left"
         heading="05  Str!ng">
 
-        <span>선과 연결로 이미지를 재구성하는 알고리즘 아트.</span>
+        <span className="relative inline-block after:content-[''] after:absolute after:inset-x-0 after:bottom-0 after:h-[22px] after:bg-[rgba(157,157,197,0.39)] after:-z-10 relative z-10">
+        선과 연결로 이미지를 재구성하는 알고리즘 아트.</span>
         <br /><br />
         <span>수많은 실선을 일정한 규칙으로 배치해</span><br />
         <span>하나의 이미지를 만들어내는 시각적 알고리즘 실험입니다.</span><br />
@@ -182,13 +211,18 @@ export default function IwapLanding() {
         id="subproject"
         imageSrc="/images/instrument_background.jpg"
         imageAlt="instrument background"
+        {...{ imageOverlay: "시작하기" }}
+        imageOverlayLeft={"!nstrument"}
+        overlayLeftClassName = "-translate-y-7 -translate-x-30"
+        imageOverlayRight={"06"}
         side="left"
         badgeText="Touch"
         textAlign="left"
         badgeAlign="left"
         heading="06  !nstrument">
 
-        <span>손동작으로 오케스트라를 지휘하는 인터랙티브 사운드 퍼포먼스.</span>
+        <span className="relative inline-block after:content-[''] after:absolute after:inset-x-0 after:bottom-0 after:h-[22px] after:bg-[rgba(157,157,197,0.39)] after:-z-10 relative z-10">
+        손동작으로 오케스트라를 지휘하는 인터랙티브 사운드 퍼포먼스.</span>
         <br /><br />
         <span>카메라로 인식한 제스처(손바닥, 손가락, 움직임)를</span><br />
         <span>MIDI 신호로 변환하여 여러 악기의 연주를 제어합니다.</span><br />
